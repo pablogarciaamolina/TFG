8ca9c62e92c7ea224de3497ab59b682b161d22f2
Pablo <pablogarciamolina6@gmail.com>
2025-03-05 17:22:18 +0100

    [UPDATE] Updated configuration of models

--- src/models/config.py
+++ src/models/config.py
@@ -18,21 +18,28 @@ MISTRAL_API_KEY = os.environ["MISTRAL_API_KEY"]
 GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
 
 LOGISTIC_REGRESSION_CONFIG = {
-    "multi_class": "multinomial"
+    "multi_class": "multinomial",
+    "max_iter": 15000, "solver": 'sag',
+    "C": 100,
+    "random_state": 0
 }
 
 LINEAR_SVC_CONFIG = {
-    ...
+    "C": 1,
+    "random_state": 0,
 }
 
 RANDOM_FOREST_CONFIG = {
-    ...
+    "n_estimators": 15, 
+    "max_depth": 8, 
+    "max_features": 20, 
+    "random_state": 0
 }
 
 KNEIGHBORS_CONFIG = {
-    ...
+    "n_neighbors": 8
 }
 
 DECISION_TREE_CONFIG = {
-    ...
+    "max_depth": 8
 }
