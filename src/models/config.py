f58a6df9519c85a044043467162a93555bc03085
Pablo <pablogarciamolina6@gmail.com>
2025-03-06 02:14:45 +0100

    [UPDATE] Updated model configuration

--- src/models/config.py
+++ src/models/config.py
@@ -4,14 +4,14 @@ SAVING_PATH = os.path.join("models", "tabnet")
 
 TABNET_PRETRAINING_PARAMS = {
     "max_epochs": 1,
-    "batch_size": 1024
+    "batch_size": 64
 }
 
 TABNET_TRAINING_PARAMS = {
     "eval_name": ['val'],
     "eval_metric": ["accuracy"],
     "max_epochs": 1,
-    "batch_size": 1024,
+    "batch_size": 64,
 }
 
 MISTRAL_API_KEY = os.environ["MISTRAL_API_KEY"]
