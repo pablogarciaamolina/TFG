6564bdb775aa0b8c66c2a542c2ec4dec60fedf23
Pablo <pablogarciamolina6@gmail.com>
2025-03-09 03:08:06 +0100

    [UPDATE] Created a Base class for APIs of LLMs

--- src/models/llm_api.py
+++ src/models/llm_api.py
@@ -1,26 +1,50 @@
-from .config import MISTRAL_API_KEY, GEMINI_API_KEY
+from abc import ABC, abstractmethod
+from typing import Any, Optional
+
 from mistralai import Mistral
 from google import genai
 
+from .config import MISTRAL_API_KEY, GEMINI_API_KEY
 
-class MistralChat:
+class BaseChat(ABC):
+    
+    client: Any
+
+    def __init__(self, model: str):
+        
+        self.model = model
+
+    @abstractmethod
+    def ask(self, instructions: str, context: Optional[str] = None) -> dict[str, str]:
+        """
+        Interface for chat completion task
+
+        Args:
+            instructions: String containing the questions for the model
+            context: Optional string containing behavioural context for the model, in order to indicate which role or position to adopt
+
+        Returns:
+            :A dictionary containing the reply from the model as well as the exit reason of the model (control purposes). Such like:
+                {
+                    "answer": <str contining the answer from the model>,
+                    "finish_reason": <exit reason in str format>
+                }
+        """
+
+        pass
+
+
+class MistralChat(BaseChat):
     """
     Mistral assitant API following https://docs.mistral.ai/api/
     """
 
     def __init__(self, model: str = "mistral-large-latest"):
 
-        self.model=model
+        super().__init__(model=model)
         self.client = Mistral(api_key=MISTRAL_API_KEY)
 
-    def ask(self,  instruction: str, context: str|None = None) -> dict[str, str]:
-        """
-        Interface for chat completion task
-
-        Args:
-            instruction: String containing the question for the model
-            context: String containing behavioural context for the model, in order to indicate which role or position to adopt
-        """
+    def ask(self, instructions: str, context: Optional[str] = None) -> dict[str, str]:
 
         messages: list[dict] = [{
             "role": "system",
@@ -28,7 +52,7 @@ class MistralChat:
         }] if context else []
         messages.append({
             "role": "user",
-            "content": instruction
+            "content": instructions
         })
 
         response = self.client.chat.complete(
@@ -41,23 +65,17 @@ class MistralChat:
             "finish_reason": response.choices[0].finish_reason
         }
     
-class GeminiChat:
+class GeminiChat(BaseChat):
 
     def __init__(self, model: str = "gemini-2.0-pro-exp-02-05"):
         
-        self.model = model
+        super().__init__(model=model)
         self.client = genai.Client(api_key=GEMINI_API_KEY)
 
-    def ask(self, instruction: str) -> dict[str, str]:
-        """
-        Interface for chat completion task
-
-        Args:
-            instruction: String containing the question for the model
-        """
+    def ask(self, instructions: str, context: Optional[str] = None) -> dict[str, str]:
 
         response = self.client.models.generate_content(
-            model=self.model, contents=instruction
+            model=self.model, contents=context + "\n" + instructions
         )
 
         return {
