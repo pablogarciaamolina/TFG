6b27d41dfd2a8d6db8500e639f535c877aabc2a1
Pablo <pablogarciamolina6@gmail.com>
2025-04-29 20:59:22 +0200

    [UPDATE] Updated implementation of batched predicting for TabPFN

--- src/models/trainable/tabpfn.py
+++ src/models/trainable/tabpfn.py
@@ -70,7 +70,7 @@ class TabPFNModel(SklearnTrainableModel):
         logging.info("TabPFN predicting labels...")
 
         batch_size = x.shape[0] if TABPFN_PARAMS["predicting_batch_size"] == -1 else TABPFN_PARAMS["predicting_batch_size"]
-        assert isinstance(TABPFN_PARAMS["predicting_batch_size"], int) and TABPFN_PARAMS["predicting_batch_size"] > 0
+        assert isinstance(batch_size, int) and batch_size > 0
 
         logging.info(f"Starting batch prediction with batch size of {batch_size}... (Total of {x.shape[0]} samples to predict)")
         preds = []
