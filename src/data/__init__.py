6f1904638d7b6f6daed94ae10f171a03b3e64f34
Pablo <pablogarciamolina6@gmail.com>
2025-05-12 04:39:39 +0200

    [UPDATE] Reorganized data module

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,4 +1,3 @@
-from .cic_ids2017 import CICIDS2017
-from .n_baiot import N_BaIoT
+from .tabular import CICIDS2017, N_BaIoT
 from .utils import sample, balanced_sample
 from .data_augmentation import smote, TabPFNDataGenerator
